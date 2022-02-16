0b21ab456ff4076ce8a54366e43629b7328b4309
Soeking <great.from.g@gmail.com>
2022-02-16 17:51:23 +0900

    init

---
+++ src/main/kotlin/soeki/Application.kt
@@ -0,0 +1,13 @@
+package soeki
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import soeki.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+        configureSecurity()
+    }.start(wait = true)
+}
