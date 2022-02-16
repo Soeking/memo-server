3628d25be57279c0afb055330c7dcd8d97630cb8
Soeking <great.from.g@gmail.com>
2022-02-16 23:01:35 +0900

    fix

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -5,7 +5,7 @@ import plugins.configureSecurity
 import plugins.configureSerialization
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+    embeddedServer(Netty, port = System.getenv("PORT").toInt(), host = "0.0.0.0") {
         configureRouting()
         configureSerialization()
         configureSecurity()
